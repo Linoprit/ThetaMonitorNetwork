5161ed452746228aad2f3610b3d20a8b53598274
Linoprit <haraldgraef@web.de>
2023-11-26 18:40:03 +0100

    Beginning with RF24 Repeater

---
+++ Modules/NRF24Repeater/Stm32F103C8/Stm32F103C8Rep/Core/Inc/main.h
@@ -0,0 +1,103 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2023 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f1xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define LED_Pin GPIO_PIN_13
+#define LED_GPIO_Port GPIOC
+#define NRF_IRQ_DS_Pin GPIO_PIN_0
+#define NRF_IRQ_DS_GPIO_Port GPIOA
+#define NRF_IRQ_DS_EXTI_IRQn EXTI0_IRQn
+#define NRF_CE_DS_Pin GPIO_PIN_1
+#define NRF_CE_DS_GPIO_Port GPIOA
+#define NRF_CSN_DS_Pin GPIO_PIN_4
+#define NRF_CSN_DS_GPIO_Port GPIOA
+#define SPI1_SCK_DS_Pin GPIO_PIN_5
+#define SPI1_SCK_DS_GPIO_Port GPIOA
+#define SPI1_MISO_DS_Pin GPIO_PIN_6
+#define SPI1_MISO_DS_GPIO_Port GPIOA
+#define SPI1_MOSI_DS_Pin GPIO_PIN_7
+#define SPI1_MOSI_DS_GPIO_Port GPIOA
+#define NRF_CSN_US_Pin GPIO_PIN_12
+#define NRF_CSN_US_GPIO_Port GPIOB
+#define SPI2_SCK_US_Pin GPIO_PIN_13
+#define SPI2_SCK_US_GPIO_Port GPIOB
+#define SPI2_MISO_US_Pin GPIO_PIN_14
+#define SPI2_MISO_US_GPIO_Port GPIOB
+#define SPI2_MOSI_US_Pin GPIO_PIN_15
+#define SPI2_MOSI_US_GPIO_Port GPIOB
+#define NRF_IRQ_US_Pin GPIO_PIN_8
+#define NRF_IRQ_US_GPIO_Port GPIOA
+#define NRF_CE_US_Pin GPIO_PIN_11
+#define NRF_CE_US_GPIO_Port GPIOA
+#define BUTTON_1_Pin GPIO_PIN_12
+#define BUTTON_1_GPIO_Port GPIOA
+#define BUTTON_1_EXTI_IRQn EXTI15_10_IRQn
+#define EEPROM_SCL_Pin GPIO_PIN_6
+#define EEPROM_SCL_GPIO_Port GPIOB
+#define EEPROM_SDA_Pin GPIO_PIN_7
+#define EEPROM_SDA_GPIO_Port GPIOB
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
